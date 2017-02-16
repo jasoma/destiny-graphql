d2bf22b784658403ae72c98b572dd34ca8144fa9
Jason Maher <jason@jasonmaher.me>
2017-02-16 14:00:29 +0900

    fold root into Query and simplify, have Account lazy load summary data if needed

--- index.js
+++ index.js
@@ -2,25 +2,11 @@ const { graphql, buildSchema } = require('graphql');
 const _ = require('lodash');
 const fs = require('fs');
 const Query = require('./lib/query');
-const DestinyApi = require('destiny-api-client');
 
 let schema = buildSchema(fs.readFileSync('schema.gql', 'utf-8'));
-let destiny = new DestinyApi(process.env.API_KEY);
-
-let root = {
-    search(source, args, context) {
-        let selections = context.fieldNodes[0].selectionSet.selections;
-        let accountFields = _.map(selections, 'name.value');
-        return Query.search(source, accountFields);
-    },
-    account(source, args, context) {
-        console.log(args);
-        return { username: 'strombane' };
-    }
-}
 
 graphql(schema, `{
-    search(username: "strombane", platform: PSN) {
+    player(username: "strombane", platform: PSN) {
         id
         username
         platform
@@ -34,4 +20,4 @@ graphql(schema, `{
             playTime
         }
     }
-}`, root).then(r => console.log(JSON.stringify(r,null,2)));
+}`, new Query()).then(r => console.log(JSON.stringify(r,null,2)));
