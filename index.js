2eb83909d44f85cd73cb0bd32672807bae8c8190
Jason Maher <jason@jasonmaher.me>
2017-02-16 16:01:02 +0900

    add character stats to the schema

--- index.js
+++ index.js
@@ -5,28 +5,6 @@ const Query = require('./lib/query');
 
 let schema = buildSchema(fs.readFileSync('schema.gql', 'utf-8'));
 
-
-// let DestinyApi = require('destiny-api-client');
-// let destiny = new DestinyApi(process.env.DESTINY_API_KEY);
-// destiny.searchPlayer({
-//     membershipType: 2,
-//     displayName: 'strombane'
-// })
-// .then(r => destiny.accountSummary({
-//     membershipType: 2,
-//     destinyMembershipId: r[0].membershipId
-// }))
-// // .then(r => {
-
-// //     return destiny.characterSummary({
-// //         destinyMembershipId: r.data.characters[0].characterBase.membershipId,
-// //         membershipType: r.data.characters[0].characterBase.membershipType,
-// //         characterId: r.data.characters[0].characterBase.characterId
-// //     });
-// // })
-// .then(r => console.log(JSON.stringify(r, null, 2)))
-// .catch(e => console.log(e));
-
 graphql(schema, `{
     player(username: "strombane", platform: PSN) {
         id
@@ -36,6 +14,14 @@ graphql(schema, `{
         characters {
             id
             class
+            stats {
+                armor
+                recovery
+                agility
+                intellect
+                discipline
+                strength
+            }
             race
             gender
             level
