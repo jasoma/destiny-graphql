d0e47d475a0a032054e0042dcf580735ba34fea6
Jason Maher <jason@jasonmaher.me>
2017-02-23 15:46:12 +0900

    attach api and manifest to the context

--- index.js
+++ index.js
@@ -2,10 +2,25 @@ const { graphql, buildSchema } = require('graphql');
 const _ = require('lodash');
 const fs = require('fs');
 const Query = require('./lib/query');
+const FileManifest = require('./lib/file-manifest');
+const DestinyApi = require('destiny-api-client');
 
 let schema = buildSchema(fs.readFileSync('schema.gql', 'utf-8'));
+let manifest = new FileManifest();
+let destiny = new DestinyApi(process.env.DESTINY_API_KEY);
+let query = new Query();
 
-graphql(schema, `{
+function run(ql) {
+    return graphql(schema, ql, {
+        player(source, args, context) {
+            context.destiny = destiny;
+            context.manifest = manifest;
+            return query.player(source, args, context);
+        }
+    });
+}
+
+run(`{
     player(username: "strombane", platform: PSN) {
         id
         username
@@ -29,4 +44,4 @@ graphql(schema, `{
             playTime
         }
     }
-}`, new Query()).then(r => console.log(JSON.stringify(r,null,2)));
+}`).then(r => console.log(JSON.stringify(r,null,2)));
